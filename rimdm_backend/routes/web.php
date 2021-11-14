26a5fcbd3a5138f250153621186696cce98e785d
arifulislamrana <ariful.islam0683@gmail.com>
2021-11-15 03:13:32 +0600

    admin panel added

--- rimdm_backend/routes/web.php
+++ rimdm_backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashBoardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/{dashboard?}', [DashboardController::class, 'index'])
+    ->where('dashboard', 'dashboard|home')
+    ->name('dashboard');
+
+
